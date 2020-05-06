1c16a51d694ca5e631ba47101f6223c938dc6b1b
Jonathan <andriodjonathan1@gmail.com>
2020-05-05 21:21:04 -0400

    BarPlot Finished and WordCloud Updates

--- wordcloud.R
+++ wordcloud.R
@@ -3,6 +3,7 @@ library("SnowballC")
 library("wordcloud")
 library("RColorBrewer")
 library("wordcloud2")
+library(plyr)
 
 # loading in data
 airplaneData <- read.csv("/Users/jonathancanales/Desktop/FinalProjectR/Airline.csv")
@@ -18,7 +19,7 @@ neg_confidenceNumber <-  airplaneData$negativereason.confidence
 # Scatter Plot Creation
 
 
-plot(d,d)
+
 
 
 
@@ -81,23 +82,31 @@ wordcloud(words = d$word, freq = d$freq,min.freq = 1,
 
 
 # making axis
-par(mai = c(0,.6,.6,.1))
+par(mai = c(1,1,1,1))
 
 plot(x = NULL,
      y = NULL,
-     xlim = c(-1,5),
+     xlim = c(0,6),
      # Each histograph covers half of each other except for the top one. You add .5 to get enough space for
      # The top one to get enough space.
      ylim = c(-1,5),
      ann = FALSE,
      axes = FALSE)
+
+xticks <- seq(0,6,3)
+# Same as ylim
+yticks <- seq(-1,5,1)
 axis(1, at = xticks, tck = .01, lab = F, lwd = 2)
 axis(2, at = yticks,tck = .01, lab = F, lwd = 2)
+axis(3, at = xticks,tck = .01, lab = F, lwd = 2)
+axis(4, at = yticks,tck = .01, lab = F, lwd = 2)
 
 title(main="main title", sub="sub-title",
    xlab="x-axis label", ylab="y-axis label")
 
-mtext("Common Negative Words",1,line = 3)
+mtext("Neutral",1,at=3)
+mtext("Postive",1,at = 1)
+mtext("Negative",1,at=6)
 
 
 text(x, y = NULL, labels = seq_along(x$x), adj = NULL,
@@ -109,3 +118,53 @@ text(3,1,labels="Negative Words")
 
 confidenceNumbers <- airplaneData[ , c("airline_sentiment.confidence", "negativereason.confidence")]
 tweetandReason <- airplaneData[ , c("text", "airline_sentiment")]
+
+
+########
+# Linear Regression with glm Function
+glm(airlineSentiment ~ Factor 1 + factor 2, family = “binomial”)
+
+
+
+
+
+
+
+###### Getting SPecific Airline Data
+united <- airplaneData[airplaneData$airline == "United",]
+virginAmerica <- airplaneData[airplaneData$airline == "Virgin America",]
+southWest <- airplaneData[airplaneData$airline == "Southwest",]
+delta <- airplaneData[airplaneData$airline == "Delta",]
+usAirways <- airplaneData[airplaneData$airline == "US Airways",]
+american <- airplaneData[airplaneData$airline == "American",]
+
+#Getting Individual columns for negativereason
+united$negativereason
+virginAmerica$negativereason
+southWest$negativereason
+delta$negativereason
+usAirways$negativereason
+american$negativereason
+
+# Need to break this into 3 things
+united$airline_sentiment
+
+
+
+library(plyr)
+countofUnited <- count(united$airline_sentiment)
+countofVirginAmerica <- count(virginAmerica$airline_sentiment)
+countofSouthWest <- count(southWest$airline_sentiment)
+countofUSAir <- count(usAirways$airline_sentiment)
+countofAmerican <- count(american$airline_sentiment)
+
+ unitedFreq <- countofUnited$freq
+ virginAmericaFreq <- countofVirginAmerica$freq
+ SouthWestFreq <- countofSouthWest$freq
+ USAirFreq <- countofUSAir$freq
+ AmericanFreq <- countofAmerican$freq
+
+
+
+coul <- brewer.pal(5, "Set2")
+out <- barplot(dataToPlot,beside=TRUE,col=c("Red","Grey","Blue"))
